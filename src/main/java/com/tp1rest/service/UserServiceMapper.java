f2e6213af93eb8014afaedc210c53a2767c8d240
Kraimps Pierre <pier_lupo@laposte.net>
2023-07-06 14:39:23 +0200

    wip

--- src/main/java/com/tp1rest/service/UserServiceMapper.java
+++ src/main/java/com/tp1rest/service/UserServiceMapper.java
@@ -2,7 +2,9 @@ package com.tp1rest.service;
 
 import com.tp1rest.dto.UserCreateDto;
 import com.tp1rest.dto.UserDto;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserServiceMapper {
 
     public UserCreateDto createUser(UserCreateDto userDto);
